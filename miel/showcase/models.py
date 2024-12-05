d11f9d3866b73a8aa55389e9aba3fd6ed7b7c892
Tastes_like_joy <ru00012r@gmail.com>
2024-12-05 21:04:09 +0300

    premigrate commit

--- miel/showcase/models.py
+++ miel/showcase/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model() # забираем кастомную модель
+
+
+class Office(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    link_to_admin = models.CharField(null=True, blank=True) # tastes_like_joy + t.me/
+    quota = models.IntegerField(null=True, blank=True)
+    default_quota = models.IntegerField(null=True, blank=True)
+    user_quota = models.IntegerField(null=True, blank=True)
+    need_quota = models.IntegerField(null=True, blank=True)
+
+    superviser = models.OneToOneField(User, on_delete=models.SET_NULL)
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Experience(models.Model):
+    workplace = models.CharField(max_length=255, null=True, blank=True)
+    occupation = models.CharField(max_length=255, null=True, blank=True)
+    date_start = models.DateTimeField(null=True, blank=True)
+    date_end = models.DateTimeField(null=True, blank=True)
+
+
+class PersonalInfo(models.Model):
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=30)
+    contact_link = models.CharField(null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    gender = models.CharField(null=True, blank=True)
+    date_of_birth = models.DateTimeField()
+
+
+class Course(models.Model):
+    name = models.CharField()
+    progress = models.IntegerField()
+
+
+class Skill(models.Model):
+    name = models.CharField()
+
+
+class CandidateCard(User):
+    created_at = models.DateTimeField(auto_now_add=True)
+    current_workplace = models.CharField(null=True, blank=True) # где сейчас
+    current_occupation = models.CharField(null=True, blank=True) # кем сейчас
+    employment_date = models.DateTimeField(null=True, blank=True) # дата трудоустройства (авто по статусу)
+    comment = models.CharField(max_length=300)
+    favorite = models.BooleanField(default=False, blank=True)
+    archived = models.BooleanField(default=False, blank=True)
+    synopsis = models.CharField(null=True, blank=True) # ссылка на резюме
+
+    objects_card = models.IntegerField(verbose_name='Объекты', null=True, blank=True)
+    clients_card = models.IntegerField(verbose_name='Клиенты', null=True, blank=True)
+
+    invitation_to_office = models.ForeignKey(Office, null=True, blank=True, on_delete=models.SET_NULL)
+    status = models.ForeignKey(Status, null=True, blank=True, on_delete=models.SET_NULL)  # TODO доработать статусы
+    experience = models.ForeignKey(Experience, on_delete=models.SET_NULL)
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+
+    course = models.ManyToManyField('CandidateCourse')
+    skills = models.ManyToManyField('CandidateSkill')
+
+class CandidateCourse(models.Model):
+    candidate = models.ForeignKey(CandidateCard, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+
+class CandidateSkill(models.Model):
+    skill = models.ForeignKey(Skill,on_delete=models.CASCADE)
+    candidate = models.ForeignKey(CandidateCard, on_delete=models.CASCADE)
---
+++ miel/users/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, **kwargs):
+
+        if not email:
+            raise ValueError()
+
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save()
+
+        '''Добавить отправку с паролем на почту пользователю'''
+
+        return user
+
+    '''create admin and superadmin'''
+
+    def create_superuser(self, email, password, **kwargs):
+        user = self.create_user(email=email, password=password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=30, null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    contact_link = models.CharField(null=True, blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+
+
+
+
