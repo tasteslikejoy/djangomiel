304b26bc73c0c0da9e160a601779515742744f4d
leroyspb <leroy777@bk.ru>
2024-12-10 17:39:07 +0300

    JWT-токен test

--- miel/miel/urls.py
+++ miel/miel/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 import djoser
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenVerifyView, TokenRefreshView, TokenObtainPairView
 
 from showcase.views import *
 from rest_framework import routers
@@ -57,6 +58,9 @@ urlpatterns = [
     path('api/v1/personal_info_delete/<int:pk>/', PersonalInfoAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('api/v1/personal_info_card/', PersonalInfoViewSet.as_view({'get': 'list'})),
     # path('api/v1/personal_info_card/<int:pk>/', PersonalInfoViewSet.as_view({'get': 'update'})),
 
